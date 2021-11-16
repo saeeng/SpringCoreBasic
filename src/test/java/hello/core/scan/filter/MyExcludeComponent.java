b816cdb7e1d84e6aac7f06340c08a52d5da217a5
saeeng <saeeng.opt@gmail.com>
2021-11-16 23:19:04 +0900

    v6

---
+++ src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE) // 적용대상 TYPE => class
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
